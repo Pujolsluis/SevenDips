5103bcb51102574e58bc4661efd8170ede7f0a08
Luis Pujols <luispujols@hotmail.com>
2017-09-19 21:08:40 -0400

    ADD "Created the AuthenticationActivity, with its corresponding Login and SignUp Fragments"

---
+++ app/src/main/java/com/nopeia/sevendips/authentication/AuthenticationActivity.java
@@ -0,0 +1,15 @@
+package com.nopeia.sevendips.authentication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.nopeia.sevendips.R;
+
+public class AuthenticationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+    }
+}
